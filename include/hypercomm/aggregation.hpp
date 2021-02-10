d39ae6e9e63d39c0c69c6144934642920f5bc021
Justin Szaday <wingpad@gmail.com>
2021-02-10 15:54:17 -0600

    rearrange things slightly

--- include/hypercomm/aggregation.hpp
+++ include/hypercomm/aggregation.hpp
@@ -13,54 +13,17 @@ using endpoint_id_t = std::size_t;
 using endpoint_registry_t = std::vector<endpoint_fn_t>;
 using msg_queue_t = std::deque<CkMarshalledMessage>;
 
+CkpvExtern(int, _bundleIdx);
+extern endpoint_id_t register_endpoint_fn(const endpoint_fn_t& fn, bool nodeLevel);
+
 template <typename... Ts>
 struct aggregator;
 
 namespace {
-CkpvDeclare(int, _bundleIdx);
-CkpvDeclare(endpoint_registry_t, endpoint_registry_);
-
-void _bundle_handler(void* msg) {
-  envelope* env = static_cast<envelope*>(msg);
-  PUP::fromMem p(EnvToUsr(env));
-  int nodeLevel, idx, nMsgs;
-  p | nodeLevel;
-  p | idx;
-  p | nMsgs;
-  const auto& reg = nodeLevel ? CkpvAccessOther(endpoint_registry_, 0)
-                              : CkpvAccess(endpoint_registry_);
-  if (static_cast<std::size_t>(idx) >= reg.size()) {
-    CkAbort(
-        "Invalid endpoint id, %d. [nodeLevel=%d, node=%d, pe=%d, size=%d]\n",
-        idx, nodeLevel, CkMyNode(), CkMyPe(), env->getTotalsize());
-  }
-  const auto& fn = reg[idx];
-  for (auto i = 0; i < nMsgs; i++) {
-    CkMarshalledMessage m;
-    p | m;
-    fn(m.getMessage());
-  }
-  QdProcess(nMsgs);
-  CmiFree(env);
-}
-
 template <typename... Ts>
 static void _on_condition(void* self) {
   static_cast<aggregator<Ts...>*>(self)->on_cond();
 }
-}
-
-void initialize(void) {
-  CkpvInitialize(endpoint_registry_t, endpoint_registry_);
-  CkpvInitialize(int, _bundleIdx);
-  CkpvAccess(_bundleIdx) = CmiRegisterHandler((CmiHandler)_bundle_handler);
-}
-
-endpoint_id_t register_endpoint_fn(const endpoint_fn_t& fn, bool nodeLevel) {
-  auto& reg = nodeLevel ? CkpvAccessOther(endpoint_registry_, 0)
-                        : CkpvAccess(endpoint_registry_);
-  reg.push_back(fn);
-  return reg.size() - 1;
 }
 
 template <typename... Ts>
